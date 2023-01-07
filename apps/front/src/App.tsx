ec65416041198553ac8880550ca848876ec5877a
Felix Bole <felix.bole@yahoo.fr>
2023-01-07 13:46:41 +0100

    feat: made wip page better

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavbarHandler } from './components/NavbarHandler/NavbarHandler';
 import { ResolutionHandler } from './components/ResolutionHandler/ResolutionHandler';
+import { ComingSoonPage } from './pages/ComingSoonPage/ComingSoonPage';
 import { HomePage } from './pages/HomePage/HomePage';
 import { LoaderPage } from './pages/LoaderPage/LoaderPage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
@@ -11,20 +12,20 @@ function App() {
     return (
         <Router>
             <NavbarHandler />
-			<ResolutionHandler>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/games/*" element={<GamesRouter />} />
+            <ResolutionHandler>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/games/*" element={<GamesRouter />} />
 
-					<Route path="/music" element={<>Coming soon</>} />
-					<Route path="/projects" element={<>Coming soon</>} />
+                    <Route path="/music" element={<ComingSoonPage />} />
+                    <Route path="/projects" element={<ComingSoonPage />} />
 
-					<Route path="/signup" element={<SignupPage />} />
-					<Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                    <Route path="/login" element={<LoginPage />} />
 
-					<Route path="/loader" element={<LoaderPage />} />
-				</Routes>
-			</ResolutionHandler>
+                    <Route path="/loader" element={<LoaderPage />} />
+                </Routes>
+            </ResolutionHandler>
         </Router>
     );
 }
---
+++ apps/front/src/pages/ComingSoonPage/ComingSoonPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styles from './ComingSoonPage.module.scss';
+
+type ComingSoonPageProps = {
+
+}
+
+export const ComingSoonPage = (props: ComingSoonPageProps) => {
+  return <div className={Styles.ComingSoonPage}>
+    <h1>Work in progress</h1>
+    <p>This page is currently in the works, please come back later.</p>
+  </div>;
+};
